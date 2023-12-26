c1bad89653cc7d0adb0257449800822694f9d625
bezbrain <belloquadriolawale@gmail.com>
2023-12-26 12:59:35 +0100

    Hash password and sign a user in the user model

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,4 +1,7 @@
+const UserCollection = require("../models/Users");
+
 const register = async (req, res) => {
+  const user = await UserCollection.create(req.body);
   res.send("Registration");
 };
 
