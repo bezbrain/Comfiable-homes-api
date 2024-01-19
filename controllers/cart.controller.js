e20e7a6aff25aac3564ab86af94794c237f485ee
bezbrain <belloquadriolawale@gmail.com>
2024-01-19 19:15:50 +0100

    Write the logic to increase cart

--- controllers/cart.controller.js
+++ controllers/cart.controller.js
@@ -4,6 +4,7 @@ const { StatusCodes } = require("http-status-codes");
 const NotFoundError = require("../errors/not-found");
 const ConflictError = require("../errors/conflict");
 const UnauthenticatedError = require("../errors/unauthenticated");
+const BadRequestError = require("../errors/bad-request");
 
 // ADD TO CART
 const addToCart = async (req, res) => {
@@ -113,9 +114,43 @@ const deleteAll = async (req, res) => {
   });
 };
 
+// INCREASE CART ITEM
+const cartIncrease = async (req, res) => {
+  const {
+    params: { itemId },
+    user: { userId },
+  } = req;
+
+  // Fetch one item so as to increase its counter
+  const item = await CartCollection.findOne({ _id: itemId, createdBy: userId });
+  if (!item) {
+    throw new NotFoundError(`Item with the id ${itemId} not found`);
+  }
+
+  const increaseCounter = item.counter + 1;
+
+  // Update the counter with the updated counter
+  const updateCounter = await CartCollection.findOneAndUpdate(
+    { _id: itemId, createdBy: userId },
+    { ...req.body, counter: increaseCounter },
+    { new: true, runValidations: true }
+  );
+
+  if (!updateCounter) {
+    throw new NotFoundError(`Item with the id ${itemId} not found`);
+  }
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    message: "Counter increased",
+    updateCounter,
+  });
+};
+
 module.exports = {
   addToCart,
   getCartItems,
   deleteFromCart,
   deleteAll,
+  cartIncrease,
 };
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -6,11 +6,15 @@ const {
   getCartItems,
   deleteFromCart,
   deleteAll,
+  cartIncrease,
 } = require("../controllers/cart.controller");
 
 router.post("/addToCart", addToCart);
 router.get("/getCartItems", getCartItems);
 router.delete("/deleteCart/:itemId", deleteFromCart);
 router.delete("/deleteAll", deleteAll);
+
+// Increasers
+router.get("/increaseItem/:itemId", cartIncrease);
 
 module.exports = router;
